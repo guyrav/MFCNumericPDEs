55b58dae8ce0e8e149bbf901e7578e818253872b
graveh <graveh@pgr.reading.ac.uk>
2025-10-31 16:03:35 +0000

    Add leapfrog and semispectral schemes, add __str__() method to schemes, some renaming

--- schemes.py
+++ schemes.py
@@ -25,8 +25,12 @@ class Scheme(object):
         pass
 
 
+    def __str__(self):
+        return "Unknown scheme"
+
+
 class ForwardTimeScheme(Scheme):
-    def apply_step(self, params: NumericalSchemeParams, current_state: np.ndarray[float]):
+    def _apply_step(self, params: NumericalSchemeParams, current_state: np.ndarray[float]):
         """Apply one forward time step, putting the output back in the current state."""
         pass
 
@@ -43,7 +47,7 @@ class ForwardTimeScheme(Scheme):
             history[0] = initial_condition
 
         for n in range(params.nt):
-            self.apply_step(params, u)
+            self._apply_step(params, u)
 
             if store_u:
                 history[n + 1] = np.pad(u, [(0, 1)], mode='wrap') if has_padding else u
@@ -58,7 +62,7 @@ class BurgersFTCS(ForwardTimeScheme):
         apply_step: Apply one forward time step, putting the output back in the current state.
                     Overridden from parent class.
     """
-    def apply_step(self, params: ViscousParams, current_state: np.ndarray[float]):
+    def _apply_step(self, params: ViscousParams, current_state: np.ndarray[float]):
         c = params.dtdx
         d = params.d
 
@@ -71,6 +75,9 @@ class BurgersFTCS(ForwardTimeScheme):
     def minimal_stable_nt(nx, T: float, L: float, nu: float):
         return int(np.ceil(nu * L / (T ** 2))) * (nx ** 2)
 
+    def __str__(self):
+        return "Burgers, FTCS"
+
 
 class AdvectionDiffusionFTCS(ForwardTimeScheme):
     """Forward-time centered-space numerical scheme for the linear advection-diffusion equation.
@@ -79,7 +86,7 @@ class AdvectionDiffusionFTCS(ForwardTimeScheme):
         apply_step: Apply one forward time step, putting the output back in the current state.
                     Overridden from parent class.
     """
-    def apply_step(self, params: AdvectionDiffusionParams, current_state: np.ndarray[float]):
+    def _apply_step(self, params: AdvectionDiffusionParams, current_state: np.ndarray[float]):
         c = params.dtdx
         d = params.d
         v = params.v
@@ -89,9 +96,16 @@ class AdvectionDiffusionFTCS(ForwardTimeScheme):
         current_state -= (c * v * 0.5 * (shifted_right - shifted_left)
                           - d * (shifted_right - 2 * current_state + shifted_left))
 
+    @staticmethod
+    def minimal_stable_nt(nx, T: float, L: float, nu: float):
+        return int(np.ceil(nu * L / (T ** 2))) * (nx ** 2)
 
-class BurgersSpectral(Scheme):
-    """Numerical scheme for solving the Burgers equation by Fourier analysis."""
+    def __str__(self):
+        return "Linear advection-diffusion, FTCS"
+
+
+class AdvectionDiffusionSpectral(Scheme):
+    """Numerical scheme for solving the linear advection-diffusion equation by Fourier analysis."""
     @staticmethod
     def _solve(params, k, u_0, t):
         return np.fft.ifft(np.fft.fft(u_0) * np.exp(-(params.nu * k**2 + 1j * params.v * k) * t))
@@ -122,3 +136,92 @@ class BurgersSpectral(Scheme):
             return solution[-1]
         else:
             return solution
+
+    def __str__(self):
+        return "Linear advection-diffusion, spectral time & space"
+
+
+class Leapfrog(Scheme):
+    def __init__(self, first_step_scheme: Scheme):
+        self.first_step_scheme = first_step_scheme
+
+    def _apply_first_step(self, first_step_params: NumericalSchemeParams,
+                    previous_state: np.ndarray[float],
+                    current_state: np.ndarray[float]):
+        previous_state[:] = current_state[:]
+        self.first_step_scheme(first_step_params, current_state)
+
+    def _apply_step(self, params: NumericalSchemeParams,
+                    previous_state: np.ndarray[float],
+                    current_state: np.ndarray[float]):
+        pass
+
+    def _get_first_step_params(self, params: NumericalSchemeParams) -> NumericalSchemeParams:
+        pass
+
+    def __call__(self, params: NumericalSchemeParams, initial_condition: np.ndarray[float], history=None):
+        """Evolve the initial condition over time using the numerical scheme."""
+        has_padding = (initial_condition.shape[0] > params.nx)  # Assumed either nx or nx + 1
+
+        u = np.zeros(params.nx, dtype=float)
+        u_prev = np.zeros_like(u)
+        u[:] = initial_condition[:-1] if has_padding else initial_condition[:]
+
+        store_u = (history is not None)
+
+        if store_u:
+            history[0] = initial_condition
+
+        if params.nt > 0:
+            self._apply_first_step(self._get_first_step_params(params), u_prev, u)
+
+            if store_u:
+                history[1] = np.pad(u, [(0, 1)], mode='wrap') if has_padding else u
+
+        for n in range(1, params.nt):
+            self._apply_step(params, u_prev, u)
+
+            if store_u:
+                history[n + 1] = np.pad(u, [(0, 1)], mode='wrap') if has_padding else u
+
+        return np.pad(u, [(0, 1)], mode='wrap') if has_padding else u
+
+
+class BurgersLeapfrog(Leapfrog):
+    def _get_first_step_params(self, params: ViscousParams) -> NumericalSchemeParams:
+        return ViscousParams(params.dt, params.L, 1, params.nx, params.nu)
+
+    def _apply_step(self, params: ViscousParams,
+                    previous_state: np.ndarray[float],
+                    current_state: np.ndarray[float]):
+        c = params.dtdx
+        d = params.d
+
+        shifted_right = np.roll(current_state, -1)
+        shifted_left = np.roll(current_state, 1)
+        previous_shifted_right = np.roll(previous_state, -1)
+        previous_shifted_left = np.roll(previous_state, 1)
+        temp = current_state.copy()
+        current_state[:] = previous_state - 2 * (c * 0.5 * current_state * (shifted_right - shifted_left)
+                                                 - d * (previous_shifted_right - 2 * previous_state + previous_shifted_left))
+        previous_state[:] = temp
+
+    def __str__(self):
+        return "Burgers, CTCS with FT diffusion"
+
+
+class BurgersSemiSpectral(ForwardTimeScheme):
+    def _apply_step(self, params: ViscousParams, current_state: np.ndarray[float]):
+        c = params.dtdx
+        d = params.d
+
+        u_hat = np.fft.fft(current_state)
+        k = 2 * np.pi * np.fft.fftfreq(params.nx, params.dx)
+
+        u_x = np.real(np.fft.ifft(1j * k * u_hat))
+        u_xx = np.real(np.fft.ifft(- k**2 * u_hat))
+
+        current_state -= params.dt * (current_state * u_x - params.nu * u_xx)
+
+    def __str__(self):
+        return "Burgers, FT spectral in space"
