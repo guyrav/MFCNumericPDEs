ead609475ee853643dfad8526ff5ed1db73f14dc
graveh <graveh@pgr.reading.ac.uk>
2025-10-31 16:02:40 +0000

    Add plot comparing evolution of two schemes

--- plots.py
+++ plots.py
@@ -40,12 +40,41 @@ def plot_evolution(t: np.ndarray[float], x: np.ndarray[float], u: np.ndarray[flo
     plt.show()
 
 
+def plot_evolution_comparison(t: np.ndarray[float], x: np.ndarray[float],
+                              u_a: np.ndarray[float], u_b: np.ndarray[float],
+                              title: str, label_a: str, label_b: str):
+    u_0_min, u_0_mean, u_0_max = u_a[0].min(), u_a[0].mean(), u_a[0].max()
+    alpha = 1.4
+    ylims = [alpha * u_0_min + (1 - alpha) * u_0_mean, alpha * u_0_max + (1 - alpha) * u_0_mean]
+
+    plt.plot(x, u_a[0], 'k', label='Initial Condition')
+    plt.title(title)
+    plt.legend(loc='best')
+    plt.ylabel('velocity')
+    plt.axhline(0, linestyle=':', color='black')
+    plt.ylim(ylims)
+    plt.pause(0.5)
+
+    for t_n, u_a_n, u_b_n in zip(t, u_a, u_b):
+        plt.cla()
+        plt.plot(x, u_a_n, 'b', label=f'{label_a}, Time {t_n:.2f}')
+        plt.plot(x, u_b_n, 'r', label=f'{label_b}, Time {t_n:.2f}')
+        plt.title(title)
+        plt.legend(loc='best')
+        plt.ylabel('velocity')
+        plt.xlabel('x')
+        plt.ylim(ylims)
+        plt.pause(3. / t.size)
+
+    plt.show()
+
+
 def plot_stability_contours(dxs, diverging_dts, diverging_nus, title: str):
-    cmap = matplotlib.cm.get_cmap('brg')
+    cmap = matplotlib.cm.get_cmap('spring')
     color_values = np.linspace(0, 1, len(dxs))
 
     for dx, dts, nus, color in zip(dxs, diverging_dts, diverging_nus, color_values):
-        plt.scatter(dts, nus, color=cmap(color), label=f"dx = {dx:.2f}")
+        plt.scatter(dts, nus, color=cmap(color), s=4, label=f"dx = {dx:.2f}")
 
     plt.legend()
     plt.title(title)
@@ -72,49 +101,6 @@ def plot_mass_evolution(t, m):
     plt.xlabel('t')
     plt.ylim([-0.01, 0.01])
     plt.show()
-
-
-# TODO: Ask Andrea whether this is worth doing
-# def plot_variation_evolution(dt, dx, nt, nx, T, L, nu):
-#     """Plot the evolution of total variation for different spatial resolutions and viscosity values.
-#     Variability is computed as V(t) = (V0 - Σ |u(x, t)- u(x-1,t)|) / V0, with V0 = Σ |u0(x, t)- u0(x-1,t)|
-#
-#     Parameters
-#     ----------
-#     dt, dx, nt, nx, T, L, nu : (float or int) Parameters.
-#
-#     """
-#     dx_values = [dt/2, dt, 1.5*dt, 2*dt, 10*dt]
-#     nx_values = [40*nx, 20*nx, 15*nx, 10*nx, 5*nx]
-#     nu_values = [nu, nu*2, nu*10]
-#     time = np.linspace(0.0,T,nt+1)
-#
-#     for viscosity in nu_values:
-#         for  i in range(len(dx_values)):
-#             x,p = make_params(dt, dx_values[i], nt, nx_values[i], viscosity)
-#             u0 = initiate(x,p,1)
-#             u_all = FTCS (x, u0, **p, store_u=True)
-#
-#             #spatial variability of initial condition
-#             shifted_left = np.roll(u0, 1)
-#             diff = abs(u0 - shifted_left)
-#             variability0 = sum(diff)
-#             variability_all = []
-#
-#             for u in u_all:
-#                 variability = total_variation_periodic(u)
-#                 #variability_all.append(variability)
-#                 variability_all.append((variability - variability0)/variability0)
-#
-#             plt.plot(time,variability_all,marker='o', markersize=1, label=f'dx= {dx_values[i]:.2f}, nu = {viscosity:.2f}')
-#             plt.legend(loc='best')
-#             plt.title(f'Spatial absolute variation over time, for dt = {dt:.2f} ')
-#             plt.ylabel('Spatial cumulated differences')
-#             plt.xlabel('t')
-#             plt.xlim([0,L])
-#             plt.ylim([-4, 10])
-#             plt.pause(0.5)
-#     plt.show()
 
 
 def plot_linearized_stability(c, d, v, log_min, log_max, title: str):
